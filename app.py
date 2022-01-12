f05381107d06f94a8b264d754d2fdd385ef3ab81
Варвара <drozdik1981@gmail.com>
2022-01-12 15:56:30 +0500

    шаг 2

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, send_from_directory
-# from functions import ...
+from functions import *
 
 POST_PATH = "posts.json"
 UPLOAD_FOLDER = "uploads/images"
@@ -9,22 +9,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def page_index():
-    pass
+    return render_template('index.html', tags=get_tags(read_json(POST_PATH)))
 
-
-@app.route("/tag")
-def page_tag():
-    pass
-
-
-@app.route("/post", methods=["GET", "POST"])
-def page_post_create():
-    pass
-
-
-@app.route("/uploads/<path:path>")
-def static_dir(path):
-    return send_from_directory("uploads", path)
+#
+# @app.route("/tag")
+# def page_tag():
+#     pass
+#
+#
+# @app.route("/post", methods=["GET", "POST"])
+# def page_post_create():
+#     pass
+#
+#
+# @app.route("/uploads/<path:path>")
+# def static_dir(path):
+#     return send_from_directory("uploads", path)
 
 
 app.run()
--- functions.py
+++ functions.py
@@ -1,1 +1,22 @@
 import json
+import pprint
+
+
+def read_json(filename):
+    with open(filename, encoding='utf-8') as f:
+        return json.load(f)
+
+
+def get_tags(data):
+    results = set()
+
+    for record in data:
+        content = record['content']
+        words = content.split()
+        for word in words:
+            if word.startswith('#'):
+                results.add(word[1:])
+    return results
+
+# pprint.pprint(read_json('posts.json'))
+# print(get_tags(read_json('posts.json')))
